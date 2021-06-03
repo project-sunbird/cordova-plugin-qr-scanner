996925c755f387c90a89ef7ef1f835f6125badb3
Pace Wisdom <pacewisdom@Paces-MacBook-Pro.local>
2021-06-03 14:16:19 +0530

    add new commit.

--- src/android/ios/QRScanner.swift
+++ src/android/ios/QRScanner.swift
@@ -4,15 +4,16 @@ import Foundation
 @objc(QRScanner) class QRScanner : CordovaPlugin { 
 
 
+// startScanner Method.
     @objc
     func startScanner(_ command: CDVInvokedUrlCommand) {
     
     }
+    // stopScanner Method.
     @objc
     func stopScanner(_ command: CDVInvokedUrlCommand) {
 
     }
-
 
        
 
