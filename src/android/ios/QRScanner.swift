a4a0ff2a73c9b4b0690dc3836e84211e1d59e0b0
Pace Wisdom <pacewisdom@Paces-MacBook-Pro.local>
2021-06-04 15:17:59 +0530

    Added method.

--- src/android/ios/QRScanner.swift
+++ src/android/ios/QRScanner.swift
@@ -2,15 +2,23 @@ import Foundation
 
 
 @objc(QRScanner) class QRScanner : CordovaPlugin { 
-
+let screenTitle = ""
+let displayText = ""
+let displayTextColor ""
+let buttonText = ""
+let showButton = ""
+let isRtl = ""
 
 // startScanner Method.
-    @objc
+    @objc(startScanner:screenTitle, displayText, displayTextColor,
+    buttonText, showButton, isRtl, success, error)
     func startScanner(_ command: CDVInvokedUrlCommand) {
-    
+
+
     }
+
     // stopScanner Method.
-    @objc
+    @objc(stopScanner:success, error)
     func stopScanner(_ command: CDVInvokedUrlCommand) {
 
     }
