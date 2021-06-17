b187aef2d318242d683fa38615686019838af6ad
dineshkokare56 <81946841+dineshkokare56@users.noreply.github.com>
2021-06-17 17:36:31 +0530

    added coded.

--- src/ios/QRScannerPlugin.swift
+++ src/ios/QRScannerPlugin.swift
@@ -1,15 +1,17 @@
 import Foundation
 import AVFoundation
 
-
 @author DineshKokare
-
 
 @objc(QRScannerPlugin) class QRScannerPlugin : CordovaPlugin {   
 
 let ACTION_QR_SCANNER = "qrScanner"	
 let START_SCANNING = "startScanner"
 let STOP_SCANNING = "stopScanner"
+
+ private  var  mScanDialog:String? = nil
+ private  var decoratedBarcodeView:String? = nil
+ private  var  themeSelected:String?
 
 func  boolean execute(Action:String, JSONArray:[args]) -> bool{
 	 if (action.equals(ACTION_QR_SCANNER)){
@@ -28,121 +30,65 @@ func  boolean execute(Action:String, JSONArray:[args]) -> bool{
 
 }
 
-func getIdOfResource(String name, String resourceType){
+func getIdOfResource(_ command: CDVInvokedUrlCommand){
+    var pluginResult:CDVPluginResult = CDVPluginResult.init(status: CDVCommandStatus_ERROR)
+        let resourceType = command.arguments[0] as? String ?? ""
+        let  name = command.arguments[1] as? String ?? ""
 return cordova.getActivity().getResources().getIdentifier(name, resourceType,
 cordova.getActivity().getApplicationInfo().packageName);
 }
 
-func showScanDialog(JSONArray args){
-	
-	 stopScanner();
-	  if ( args.isFinishing()){
-     
-      		 return;
-       
- 	  }
+func sendErrorCode(command: CDVInvokedUrlCommand, error: QRScannerError){
+        let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: error.rawValue)
+        commandDelegate!.send(pluginResult, callbackId:command.callbackId)
+    }
 
-	let  appSharedPreferences = cordova.getActivity().getSharedPreferences("org.ekstep.genieservices.preference_file", Context.MODE_PRIVATE)
-	let  themeSelected = appSharedPreferences.getString("current_selected_theme", "JOYFUL");
-	
-	let  String title = args.optString(1, "Scan QR Code");
-    displayText.text = args.optString(2, "Point your phone to the QR code to scan it");
-    displayText.backgroundcolor = args.optString(3, "#0b0b0b");
-     buttonText.text = args.optString(4, "I don't have a QR Code");
-	var showButton:Bool = args.optBoolean(5, false);
-	var boolean isRtl = args.optBoolean(6, false);
-	 
-	Outlet-->var view = ""
-        Outlet-->var Toolbar toolbar
-	Outlet-->var ImageView imageView 
-
-	runOnUiThread(new Runnable(){
-
-		  if (!themeSelected.equalsIgnoreCase("JOYFUL")) {
-			  view.setBackgroundColor(Color.parseColor("#f3f3f5"));
-
-			  View view1 = view.findViewById(getIdOfResource("walkthrough_scan", "id"));
-
-			  toolbar.setBackgroundColor(Color.parseColor("#f3f3f5"));
-
-			   view1.setBackgroundColor(Color.parseColor("#f3f3f5"));
-			   View oldScanLogoBackGround = view.findViewById(getIdOfResource("walkthrough_scan_image_layout", "id"));
-			   oldScanLogoBackGround.setBackgroundColor(Color.parseColor("#f3f3f5"));
-			  imageView = view.findViewById(getIdOfResource("joyful_new_scan_logo", "id"));
-			  imageView.setVisibility(View.GONE);
-			  ImageView oldScanScanLogo = view.findViewById(getIdOfResource("default_scan_logo", "id"));
-   
-                          oldScanScanLogo.setVisibility(View.VISIBLE);
-
-		  }
-			 toolbar.setTitle(title);
-
-		  if (isRtl) {
-                   
-			 view.setLayoutDirection(View.LAYOUT_DIRECTION_RTL);
-    
-              		  toolbar.setNavigationIcon(getIdOfResource("ic_action_arrow_right", "drawable"));
-    
-            } else {
-   
-           
-    		  toolbar.setNavigationIcon(getIdOfResource("ic_action_arrow_left", "drawable"));
-    
-            }
-
-		 toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-      
-              @Override
-                   
-	    public void onClick(View v) {
-        
-               		 callbackContext.success("cancel_nav_back");
-             
-      		 }
-               
- 		});
-		
-        }
-
-}
 
 // startScanner Method.
     @objc(startScanner:)
     
 func startScanner(_ command: CDVInvokedUrlCommand) {
-       
-let screenTitle : String? = "Scan QR Code"
+    var pluginResult:CDVPluginResult = CDVPluginResult.init(status: CDVCommandStatus_ERROR)
+        let showButton = command.arguments[5] as? Bool ?? false
+        let isRtl = command.arguments[4] as? Bool ?? false
+        let screenTitle = command.arguments[0] as? String ??  "Scan QR Code"
+        let displayText = command.arguments[1] as? String ?? "Point your phone to the QR code to scan it"
+       let buttonText = command.arguments[2] as? String ?? "I don't have a QR Code"
+       let displayTextColor = command.arguments[3] as? String ?? "0b0b0b"
+      
+      self.view = view(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height))
+      self.view.autoresizingMask = [.flexibleWidth, .flexibleHeight];
 
-let displayText : String? = "Point your phone to the QR code to scan it"
+     self.Toolbar = Toolbar(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height))
+      self.Toolbar.autoresizingMask = [.flexibleWidth, .flexibleHeight];
 
-let displayTextColor: String? = "#0b0b0b"
+	var ImageView = ImageView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height))
+      self.ImageView.autoresizingMask = [.flexibleWidth, .flexibleHeight];
 
-let buttonText : String? = "I don't have a QR Code"
+}
 
-let showButton : Boolean? = false
-
-let isRtl :Boolean? = false
-
-if(self.prepScanner(command: command)){
-            nextScanningCommand = command
-            scanning = true
+	runOnUiThread(new Runnable(){  
+ 	
+		
         }
+
+
        
 
     }
+    
 
     // stopScanner Method.
     @objc(stopScanner:)
     func stopScanner(_ command: CDVInvokedUrlCommand) {
-     (self.prepScanner(command: command)){
-            scanning = false
-            if(nextScanningCommand != nil){
-                self.sendErrorCode(command: nextScanningCommand!, error: QRScannerError.scan_canceled)
+     cordova.getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+               
             }
-            self.getStatus(command)
-        }
+        });
 
     }
   
 
-}
+
