6132e4e4339870c2102eacb9ff92c48b82c43a06
dineshkokare56 <81946841+dineshkokare56@users.noreply.github.com>
2021-06-17 10:55:38 +0530

    Update QRScannerPlugin.swift

--- src/ios/QRScannerPlugin.swift
+++ src/ios/QRScannerPlugin.swift
@@ -1,9 +1,9 @@
 import Foundation
 import AVFoundation
 
+
 @author DineshKokare
 
-import Foundation
 
 @objc(QRScannerPlugin) class QRScannerPlugin : CordovaPlugin {   
 
@@ -28,6 +28,83 @@ func  boolean execute(Action:String, JSONArray:[args]) -> bool{
 
 }
 
+func getIdOfResource(String name, String resourceType){
+return cordova.getActivity().getResources().getIdentifier(name, resourceType,
+cordova.getActivity().getApplicationInfo().packageName);
+}
+
+func showScanDialog(JSONArray args){
+	
+	 stopScanner();
+	  if ( args.isFinishing()){
+     
+      		 return;
+       
+ 	  }
+
+	let  appSharedPreferences = cordova.getActivity().getSharedPreferences("org.ekstep.genieservices.preference_file", Context.MODE_PRIVATE)
+	let  themeSelected = appSharedPreferences.getString("current_selected_theme", "JOYFUL");
+	
+	let  String title = args.optString(1, "Scan QR Code");
+    displayText.text = args.optString(2, "Point your phone to the QR code to scan it");
+    displayText.backgroundcolor = args.optString(3, "#0b0b0b");
+     buttonText.text = args.optString(4, "I don't have a QR Code");
+	var showButton:Bool = args.optBoolean(5, false);
+	var boolean isRtl = args.optBoolean(6, false);
+	 
+	Outlet-->var view = ""
+        Outlet-->var Toolbar toolbar
+	Outlet-->var ImageView imageView 
+
+	runOnUiThread(new Runnable(){
+
+		  if (!themeSelected.equalsIgnoreCase("JOYFUL")) {
+			  view.setBackgroundColor(Color.parseColor("#f3f3f5"));
+
+			  View view1 = view.findViewById(getIdOfResource("walkthrough_scan", "id"));
+
+			  toolbar.setBackgroundColor(Color.parseColor("#f3f3f5"));
+
+			   view1.setBackgroundColor(Color.parseColor("#f3f3f5"));
+			   View oldScanLogoBackGround = view.findViewById(getIdOfResource("walkthrough_scan_image_layout", "id"));
+			   oldScanLogoBackGround.setBackgroundColor(Color.parseColor("#f3f3f5"));
+			  imageView = view.findViewById(getIdOfResource("joyful_new_scan_logo", "id"));
+			  imageView.setVisibility(View.GONE);
+			  ImageView oldScanScanLogo = view.findViewById(getIdOfResource("default_scan_logo", "id"));
+   
+                          oldScanScanLogo.setVisibility(View.VISIBLE);
+
+		  }
+			 toolbar.setTitle(title);
+
+		  if (isRtl) {
+                   
+			 view.setLayoutDirection(View.LAYOUT_DIRECTION_RTL);
+    
+              		  toolbar.setNavigationIcon(getIdOfResource("ic_action_arrow_right", "drawable"));
+    
+            } else {
+   
+           
+    		  toolbar.setNavigationIcon(getIdOfResource("ic_action_arrow_left", "drawable"));
+    
+            }
+
+		 toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+      
+              @Override
+                   
+	    public void onClick(View v) {
+        
+               		 callbackContext.success("cancel_nav_back");
+             
+      		 }
+               
+ 		});
+		
+        }
+
+}
 
 // startScanner Method.
     @objc(startScanner:)
